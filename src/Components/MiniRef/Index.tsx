b725a003370744bc412ae653cd888501fb61be98
Pedro Lucca Medeiros Miranda <pedroluccaprofissional@gmail.com>
2024-03-20 12:10:43 -0300

    Ultimo commit

---
+++ src/Components/MiniRef/Index.tsx
@@ -0,0 +1,17 @@
+import './Style.css';
+
+type props = {
+    img: string,
+    title: string
+}
+
+const Card = (props:props) => {
+    return(
+        <div className="card">
+            <img id="cardImg" src={props.img}/>
+            <h1 id="cardTxt">{props.title}</h1>
+        </div>
+    )
+}
+
+export default Card;
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client'
 import Header from './Components/Header/index.tsx'
 import SearchBar from './Components/SearchBar/index.tsx'
 import './index.css'
+import Carroussel from './Components/Carroussel/Index.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
   <Header/>
   <main>
     <SearchBar/>
+    <Carroussel/>
   </main>
   </React.StrictMode>,
 )
